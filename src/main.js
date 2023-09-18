94f5ffc56a803c3a5f8d410c1ed80db4235821ac
zhangjian <15555117523@163.com>
2023-09-18 09:56:57 +0800

    修改配置

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
-import App from './App.vue'
+import 'normalize.css';
 
-Vue.config.productionTip = false
+import ElementUI from 'element-ui'; // 2.1引入结构
+import 'element-ui/lib/theme-chalk/index.css'; // 2.2引入样式
+import App from './App.vue';
+// import Print from 'vue-print-nb';
+// import  VueDragResize from 'vue-drag-resize'
+
+Vue.use(ElementUI); // 3.安装
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: '#app',
+  render: h => h(App)
+});
